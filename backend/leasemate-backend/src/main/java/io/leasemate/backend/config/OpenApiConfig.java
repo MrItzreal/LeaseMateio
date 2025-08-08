8fa6368fe8ad131164b519fbbb682bfbd17fd6ed
MrItzreal <is.wsie@gmail.com>
2025-08-08 09:33:23 -0400

    Refactor OpenApiConfig to improve readability and add security scheme definition.
    -This security setup adds a green "Authorize" button to Swagger UI, allowing to "log in" by providing a token.

--- backend/leasemate-backend/src/main/java/io/leasemate/backend/config/OpenApiConfig.java
+++ backend/leasemate-backend/src/main/java/io/leasemate/backend/config/OpenApiConfig.java
@@ -1,15 +1,42 @@
 package io.leasemate.backend.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "LeaseMateIO API", version = "1.0", description = "This API exposes endpoints to managed properties, apartments, and tenants.", contact = @Contact(name = "Call Me Izzy!", email = "[No Personal Email]", url = "https://x.com/fullstackizzy"), license = @License(name = "MIT License", url = "https://choosealicense.com/licenses/mit/"), termsOfService = "https://x.com/fullstackizzy"), servers = @Server(url = "http://localhost:8080", description = "Server URL in Local development environment"))
+@OpenAPIDefinition(
+   info = @Info(
+      title = "LeaseMateIO API", 
+      version = "1.0", 
+      description = "This API exposes endpoints to managed properties, apartments, and tenants.", 
+   contact = @Contact(
+      name = "Call Me Izzy!", 
+      email = "[No Personal Email]", 
+      url = "https://x.com/fullstackizzy"), 
+   license = @License(
+      name = "MIT License", 
+      url = "https://choosealicense.com/licenses/mit/"), 
+      termsOfService = "https://x.com/fullstackizzy"), 
+   servers = @Server(
+      url = "http://localhost:8080", 
+      description = "Server URL in Local development environment"),
+   security = @SecurityRequirement(
+      name = "bearerAuth"
+   ))
 
+@SecurityScheme(
+    name = "bearerAuth", // A name for this security scheme
+    type = SecuritySchemeType.HTTP, // The type of security scheme
+    scheme = "bearer", // The specific scheme (bearer tokens)
+    bearerFormat = "JWT" // A hint about the token format
+ )
 public class OpenApiConfig {
   // OpenApiConfig class is just a holder for the above configuration.
 }
