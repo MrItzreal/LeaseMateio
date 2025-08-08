3ce0ebe8d102316b14f90bc4d51d1a5868a03f7f
MrItzreal <is.wsie@gmail.com>
2025-08-08 17:22:02 -0400

    Add Temporary components and Clerk JWT Session retrieval code and more:
    - Created temporary Home, Signup, Login components with a simple heading.
    - Created function to extract CLERK JWT Token Session
    - Added Clerk themes package
    - Changed metadata and svg icon
    - Deleted unused files and code

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { ClerkProvider } from "@clerk/clerk-react";
+import { BrowserRouter } from "react-router-dom";
+import { neobrutalism } from "@clerk/themes";
+import App from "./App.tsx";
+import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
+// Import your Publishable Key
+const CLERK_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!CLERK_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ClerkProvider
+      appearance={{
+        baseTheme: neobrutalism,
+      }}
+      publishableKey={CLERK_KEY}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ClerkProvider>
+  </StrictMode>
+);
